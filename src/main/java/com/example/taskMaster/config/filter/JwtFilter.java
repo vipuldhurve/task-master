f77cb1514fe29793ee0a96e1f879d3df69881e21
vipuldhurve <vipuldhurve@gmail.com>
2024-09-25 19:40:04 +0530

    formatting

--- src/main/java/com/example/taskMaster/config/filter/JwtFilter.java
+++ src/main/java/com/example/taskMaster/config/filter/JwtFilter.java
@@ -32,7 +32,7 @@ public class JwtFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         // Bearer eyadsa1ab34.cdedalasdj
-        // Gert token from request
+        // Get token from request
         String authHeader = request.getHeader("Authorization");
         String token = null;
         String username = null;
